c91d7918e6dad2a8b66ce613933f06855beef1a8
Eirik Årdal <eaardal@outlook.com>
2015-03-09 20:23:54 +0100

    Removed CSharp and Java projects

--- LibraryApp/Java/libraryapp/target/libraryapp/app/app.js
+++
@@ -1,3 +0,0 @@
-/// <reference path="references.ts" />
-var app = angular.module('libraryApp', ['ngRoute']);
-//# sourceMappingURL=app.js.map
--- LibraryApp/Java/libraryapp/target/libraryapp/app/app.ts
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="references.ts" />
-
-declare var angular : ng.IAngularStatic;
-var app: ng.IModule = angular.module('libraryApp', ['ngRoute']);
